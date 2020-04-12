21bf0298d0f806dd9379777cac69521fda213d87
dungdd-1735 <do.dinh.dung@sun-asterisk.com>
2020-04-12 21:58:05 +0700

    add default permission to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_160119) do
+ActiveRecord::Schema.define(version: 2020_04_12_144717) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2020_04_08_160119) do
     t.string "name"
     t.string "phone"
     t.text "address"
-    t.integer "permission"
+    t.integer "permission", default: 3
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
