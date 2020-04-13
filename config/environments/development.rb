a299b9ca3dfebc5c6cdab695ad44babbc85fae3e
tuanhv-0694 <49422593+tuanhv-0694@users.noreply.github.com>
2020-04-13 23:29:09 +0700

    Revert "account activation with devise gem (#13)"
    This reverts commit d22bb0807cdebaca563bfa3f1406ea4d89dbbff6.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
@@ -60,14 +60,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => ENV["GMAIL_USERNAME"],
-    :password => ENV["GMAIL_PASSWORD"],
-    :authentication => "plain",
-    :enable_starttls_auto => true,
-  }
 end
