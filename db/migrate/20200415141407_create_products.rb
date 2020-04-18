fd0d49ac7f2293d95884371e8a715a9123e4efcb
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-04-18 15:37:15 +0700

    Fix migrate database

--- db/migrate/20200415141407_create_products.rb
+++ db/migrate/20200415141407_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
       t.string :product_name, null: false, default: ""
-      t.text :detail, null: false, default: ""
+      t.text :detail, null: false
       t.integer :price, null: false
       t.integer :quantity, null: false, default: 1
       t.integer :category_id, nul: false
--- db/migrate/20200415153725_add_category_id_to_product.rb
+++ db/migrate/20200415153725_add_category_id_to_product.rb
@@ -1,6 +1,5 @@
 class AddCategoryIdToProduct < ActiveRecord::Migration[6.0]
   def change
-    add_column :products, :category_id, :integer
     add_index :products, :category_id
   end
 end
