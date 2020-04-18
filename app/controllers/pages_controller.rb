df49f69c7ff042cf2f37e38c75f87efaea218552
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-04-18 14:51:43 +0700

    fix file pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,8 @@
 class PagesController < ApplicationController
   def index
-    @product = Product.paginate(page: params[:page])
   end
 
   def home
     @product = Product.all
-  end
-
-  private
-
-  def home_params
-    params.require(:product).permit(:product_name, :detail, :price, :quantity, :slug, :category_id)
   end
 end
