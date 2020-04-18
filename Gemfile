d0b51521bd9970e2ddac85ab082180ee38a36398
dungdd-1735 <do.dinh.dung@sun-asterisk.com>
2020-04-18 14:23:46 +0700

    setting gem config

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.5'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
+gem "config"
 # Use Rubocop with AirBNB setting
 gem 'rubocop-airbnb', require: false
 # Use mysql as the database for Active Record
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   # enum
   enum permission: { admin: 1, staff: 2, customer: 3 }
   # Validates
-  validates :email, presence: true
+  validates :email, presence: true, length: { maximum: Settings.user.email.max_length }
+  validates :name, presence: true, length: { maximum: Settings.user.name.max_length }
   validates :permission, presence: true
 end
