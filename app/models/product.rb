b1920d93599ab2a3d92339c24110f48e6ba4477d
Linh Nguyen Thien <54306497+thiennlinh@users.noreply.github.com>
2020-04-20 16:32:09 +0700

    [BackEnd][User] Show Product on the Homepage (#18)
    * Add footer
    
    * Show product on homepage
    
    * fix file pages_controller
    
    * fix no.1
    
    * Create method lates
    
    * use scope latest
    
    * Fix syntax rails 6
    
    * Fix no.3

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,10 @@ class Product < ApplicationRecord
   # Validates
   validates :product_name, presence: true, length: { maximum: 35 }
   validates :detail, length: { minimum: 14, maximum: 300 }
+
+  # Method
+  scope :latest, lambda {
+                   where('created_at >= created_at or updated_at >= updated_at',
+                         created_at: Time.zone.now - 15.days, updated_at: Time.zone.now - 3.days)
+                 }
 end
