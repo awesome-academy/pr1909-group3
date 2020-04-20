99e5c6781eeee94bcbb7001ecccb2471db777f8c
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-04-20 20:39:58 +0700

    Create view user infomation

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_04_16_132452) do
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "category_name", null: false
     t.string "code", null: false
@@ -62,4 +63,5 @@ ActiveRecord::Schema.define(version: 2020_04_16_132452) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
