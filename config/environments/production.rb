6d0c801dd018fe95d70925c63a4d55e68e2d8784
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-04-21 16:31:26 +0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,13 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"],
+    :authentication => "plain",
+    :enable_starttls_auto => true,
+  }
+  config.action_mailer.default_url_options = { host: 'https://thawing-island-70100.herokuapp.com/'}
 end
