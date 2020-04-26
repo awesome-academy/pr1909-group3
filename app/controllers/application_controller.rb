fcf45e9af42b940167f9e528f7547adeda170e95
dungdd-1735 <do.dinh.dung@sun-asterisk.com>
2020-04-26 22:56:12 +0700

    add debug UI and fix rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    locale = params[:locale].to_s.strip.to_sym
-    I18n.locale = I18n.available_locales.include?(locale) ?
-      locale : I18n.default_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
   protected
