3506484c3c6bb9a9fec58e6e728f1ef66c20e90d
Linh Nguyen Thien <54306497+thiennlinh@users.noreply.github.com>
2020-04-27 18:34:24 +0700

    [Backend][User] Task search product in the homepage (#38)
    * Create function search product
    
    * Fix link_to

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'font-awesome-rails'
 gem 'rails_admin'
 gem 'rails-i18n', '~> 6.0.0'
 gem 'rails_admin-i18n'
+gem 'ransack'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root to: 'pages#index'
+  root to: 'products#index'
   devise_for :users
   resources :customers
+  resources :lists
   resources :products
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
