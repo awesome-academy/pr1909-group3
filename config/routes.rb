4808f97240556ebcb44d66f0173dfcefaa21ffcd
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-04-30 14:02:24 +0700

    Error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'lists/index'
   get 'lists/show'
+  get 'products/add_to_cart/:id', to: 'products#add_to_cart', as: 'add_to_cart'
+  delete 'products/remove_from_cart/:id', to: 'products#remove_from_cart', as: 'remove_from_cart'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'products#index'
   devise_for :users
