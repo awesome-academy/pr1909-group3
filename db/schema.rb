5a12cfbafec521053c7b22bf214d926570b2cf3d
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-05-19 19:01:17 +0700

    Refactor method update and destroy in order_details_controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_05_11_120814) do
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "category_name", null: false
     t.string "code", null: false
     t.string "parent_code"
@@ -20,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_120814) do
     t.string "slug"
   end
 
-  create_table "ckeditor_assets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "ckeditor_assets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "data_file_name", null: false
     t.string "data_content_type"
     t.integer "data_file_size"
@@ -32,7 +33,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_120814) do
     t.index ["type"], name: "index_ckeditor_assets_on_type"
   end
 
-  create_table "order_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "order_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "quantity"
     t.integer "unit_price"
     t.integer "total_price"
@@ -44,7 +45,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_120814) do
     t.index ["product_id"], name: "index_order_details_on_product_id"
   end
 
-  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "address"
     t.integer "status"
     t.integer "subtotal"
@@ -54,7 +55,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_120814) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "product_name", default: "", null: false
     t.text "detail", null: false
     t.integer "price", null: false
@@ -67,7 +68,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_120814) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
-  create_table "promotions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "promotions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "percent", null: false
     t.datetime "start_date"
     t.datetime "end_date"
@@ -77,7 +78,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_120814) do
     t.index ["product_id"], name: "index_promotions_on_product_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
