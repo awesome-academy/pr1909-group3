1dc05473f20e6e9dfc6fb2927c768ca9ebe25690
dungdd-1735 <do.dinh.dung@sun-asterisk.com>
2020-05-25 00:41:22 +0700

    fix unnecessary code

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,2 @@
 class CategoriesController < ApplicationController
-  def load_categories
-    @categories = Category.all
-    @cates_hash = {}
-    @categories.each do |category|
-      id = category.id
-      name = category.category_name
-      @cates_hash.store(name, id)
-    end
-  end
 end
