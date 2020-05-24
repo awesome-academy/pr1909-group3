406d62315855307a23072f08f281e9bb855740ec
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-05-24 12:52:45 +0700

    Fix html safe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_05_17_134307) do
-
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "category_name", null: false
     t.string "code", null: false
