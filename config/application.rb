6e637e089cd2d21fa69320593680efa0d2af38d8
dungdodinhjpvn <dungdodinhjpvn@gmail.com>
2020-06-11 19:31:53 +0700

    fix env database.yml

--- config/application.rb
+++ config/application.rb
@@ -13,15 +13,6 @@ module Pr1909Project2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      if File.exist?(env_file)
-        YAML.safe_load(File.open(env_file)).each do |key, value|
-          ENV[key.to_s] = value
-        end
-      end
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
