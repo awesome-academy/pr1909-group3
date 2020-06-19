556da5f98e4097941f9bed698e5caf8bf2d7a170
dungdodinhjpvn <dungdodinhjpvn@gmail.com>
2020-06-19 20:24:54 +0700

    add css js template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   private
-  
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
-  
+
   def default_url_options
     { locale: I18n.locale }
   end
