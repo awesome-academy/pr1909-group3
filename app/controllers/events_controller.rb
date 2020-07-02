b81770332859075a3f505e621833f46c614f290b
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-07-02 20:22:33 +0700

    Join by code

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,45 @@
+class EventsController < ApplicationController
+  before_action :set_event, except: %i(index join_by_code checkout_event)
+  before_action :correct_user, only: [:show]
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    gon.event = @event
+    @notes = @event.notes.actived
+  end
+
+  def join_by_code
+    if @event = Event.find_by_degit_code(params[:degit_code])
+      redirect_ajax event_path(@event)
+      remember_participant @event
+    else
+      # redirect ve trang dang nhap va hien thong bao
+    end
+  end
+
+  def checkout_event
+    forget_participant
+    redirect_to root_path
+  end
+
+  def notes
+    @notes = @event.notes.actived
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def correct_user
+    if current_participant.nil?
+      redirect_to root_path
+    else
+      redirect_to root_path unless current_participant.event_id == @event.id
+    end
+  end
+end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,4 +1,17 @@
 module EventsHelper
+  def remember_participant(event)
+    participant = Participant.create(event: event, name: "No Name", is_admin: false)
+    cookies.permanent.signed[:participant_id] = participant.id
+    cookies.permanent[:authen_event_token] = participant.participant_token
+    session[:participant_id] = participant.id
+  end
+
+  def forget_participant
+    cookies.permanent.signed[:participant_id] = nil
+    cookies.permanent[:authen_event_token] = nil
+    session[:participant_id] = nil
+  end
+
   def current_participant
     if (participant_id = session[:participant_id])
       @current_participant ||= Participant.find_by(id: participant_id)
