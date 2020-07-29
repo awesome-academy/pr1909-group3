68b7c87fb6fa069bb0555e30eda1234f16c139e2
thiennlinh <linhnt.B17PT118@stu.ptit.edu.vn>
2020-07-29 20:20:34 +0700

    Fix login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -10,9 +10,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def generic_callback(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+      sign_in @user, event: :authentication
       set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
-      redirect_to dashboard_event_path
+      redirect_to dashboard_root_path
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_url
